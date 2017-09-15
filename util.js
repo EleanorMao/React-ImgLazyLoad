36b0ed48ecbed29cff84b552713db02691da260f
EleanorMao <danningmao@outlook.com>
2017-09-15 17:20:50 +0800

    Create util.js

---
+++ util.js
@@ -0,0 +1,41 @@
+const _now = Date.now || function () {
+    return new Date().getTime()
+};
+
+export function throttle(func, wait) {
+    var context,
+        args,
+        result;
+    var timeout = null;
+    var previous = 0;
+
+    var later = function () {
+        previous = _now();
+        timeout = null;
+        result = func.apply(context, args);
+        if (!timeout) {
+            context = args = null
+        }
+    };
+
+    return function () {
+        var now = _now();
+
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0 || remaining > wait) {
+            clearTimeout(timeout);
+            timeout = null;
+            previous = now;
+            result = func.apply(context, args);
+            if (!timeout) {
+                context = args = null;
+            }
+        } else if (!timeout) {
+            timeout = setTimeout(later, remaining);
+        }
+        return result
+    }
+}
+
